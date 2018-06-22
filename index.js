08eea11d6f40f944b5b92b97ffbd1e1b08fac564
abbylee1 <ablee@micds.org>
2018-06-22 21:08:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function talksNeverFails() {  
+    let tails = 0;
+    
+    while (Math.random() >= 0.5) {
+      tails++
+    }
+    
+    console.log(`You got ${tails} tails in a row!`);
+  }
+  
+  talksNeverFails();
+  
